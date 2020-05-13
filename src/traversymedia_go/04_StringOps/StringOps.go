25f79ce68e577becdf94749febf9b772409a6951
subhadipraxit <subhadipraxit@gmail.com>
2020-05-13 20:08:43 +0530

    04 module added

---
+++ src/traversymedia_go/04_StringOps/StringOps.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+	s "strings"
+)
+
+var p = fmt.Println
+
+func main() {
+
+	p("String Operations : ")
+	var str string = "New String"
+	var strArray = []string{"This", "Separator,", "it?"}
+	p(s.Contains(str, "New"))
+	p(s.Repeat(str, 2))
+	p(s.ReplaceAll(str, "New", "Old"))
+	p(s.TrimSuffix(str, "ng"))
+	p(s.Join(strArray, " is "))
+	//p(s.)
+	//p(s.)
+	//p(s.)
+
+}
